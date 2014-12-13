0b7d8127f769a7f4c896fe099608be09ca1db813
Ivan Konorkin <konorkin.ivan@gmail.com>
2014-12-14 00:27:57 +0300

    Right panel + some fixes of calendar

--- desktop/js/great-task.js
+++ desktop/js/great-task.js
@@ -1,2 +1,14 @@
 $(".time-table").height($(window).height()-220);
 $(".time-table").scrollTop(238);
+$(".form-inline.add-task input").width( $(".right-panel").width() - 91 );
+
+$("#navtab a:last").click(function (e) {
+	$(".left-panel").css("position", "relative");
+	$(".right-panel").removeClass("col-lg-offset-6 col-md-offset-6");
+	$('html, body').animate({scrollTop:0}, "fast");
+});
+
+$("#navtab a:first").click(function (e) {
+	$(".left-panel").css("position", "fixed");
+	$(".right-panel").addClass("col-lg-offset-6 col-md-offset-6");
+});
