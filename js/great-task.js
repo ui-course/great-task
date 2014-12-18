5cc7deeef08ae40969a637fa112d3b3d8aa4a42a
Eugene Sharygin <eush77@gmail.com>
2014-12-18 19:54:17 +0300

    Split things into separate functions

--- js/great-task.js
+++ js/great-task.js
@@ -1,34 +1,52 @@
-$(window).resize(_.throttle(function () {
+var $leftPanel = $('.left-panel')
+  , $rightPanel = $('.right-panel');
+
+
+var fixSizes = function () {
   $(".left-panel .panel-body").height($(window).height()-220);
   $(".time-table").scrollTop(238);
-  $(".form-inline.add-task input").width( $(".right-panel").width() - 91 );
-}, 100));
-$(window).resize();
+  $(".form-inline.add-task input").width( $rightPanel.width() - 91 );
+};
 
-$("#navtab a:last").click(function (e) {
-	$(".left-panel").css("position", "relative");
-	$(".right-panel").removeClass("col-lg-offset-6 col-md-offset-6");
+
+var setUpTabs = function () {
+  $("#navtab a:last").click(function (e) {
+	$leftPanel.css("position", "relative");
+	$rightPanel.removeClass("col-lg-offset-6 col-md-offset-6");
 	$('html, body').animate({scrollTop:0}, "fast");
-});
+  });
 
-$("#navtab a:first").click(function (e) {
-	$(".left-panel").css("position", "fixed");
-	$(".right-panel").addClass("col-lg-offset-6 col-md-offset-6");
-});
+  $("#navtab a:first").click(function (e) {
+	$leftPanel.css("position", "fixed");
+	$rightPanel.addClass("col-lg-offset-6 col-md-offset-6");
+  });
+};
 
-$('.left-panel .sortable').sortable();
 
-$('.right-panel .sortable').sortable({
-  connectWith: '.right-panel .sortable'
-});
+var setUpDragAndDrop = function () {
+  $('.left-panel .sortable').sortable();
 
-$('.droppable').droppable({
-  accept: '.right-panel .task',
-  drop: function (event, ui) {
-    var template = $('.template', this).text().trim();
+  $('.right-panel .sortable').sortable({
+    connectWith: '.right-panel .sortable'
+  });
 
-    $('.list-group', this).append(_.template(template, {
-      text: ui.draggable.text()
-    }));
-  }
+  $('.droppable').droppable({
+    accept: '.right-panel .task',
+    drop: function (event, ui) {
+      var template = $('.template', this).text().trim();
+
+      $('.list-group', this).append(_.template(template, {
+        text: ui.draggable.text()
+      }));
+    }
+  });
+};
+
+
+$(function () {
+  fixSizes();
+  setUpTabs();
+  setUpDragAndDrop();
+
+  $(window).resize(_.throttle(fixSizes, 100));
 });
